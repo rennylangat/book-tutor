7429ff7cdd7d47875cdde968caa6b56eaf04e173
rennylangat <rennylngt@gmail.com>
2021-07-24 03:21:20 +0300

    Select Tutor

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -79,11 +79,120 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     )
                   ],
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                Container(
+                  width: SizeConfig.screenWidth,
+                  height: SizeConfig.screenHeight * 0.8,
+                  child: ListView(
+                    children: [
+                      TutorsDetails(
+                        tutorName: "Chinaza Akachi",
+                        subject: "Mathematics",
+                        location: "Vancouver, BC",
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(20),
+                      ),
+                      TutorsDetails(
+                        tutorName: "Chinaza Akachi",
+                        subject: "Mathematics",
+                        location: "Vancouver, BC",
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(20),
+                      ),
+                      TutorsDetails(
+                        tutorName: "Chinaza Akachi",
+                        subject: "Mathematics",
+                        location: "Vancouver, BC",
+                      ),
+                    ],
+                  ),
                 )
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TutorsDetails extends StatelessWidget {
+  final String tutorName, subject, location;
+  const TutorsDetails({
+    Key key,
+    this.tutorName,
+    this.subject,
+    this.location,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: SizeConfig.screenWidth * 0.35,
+            alignment: Alignment.center,
+            child: Container(
+              width: getProportionateScreenHeight(100),
+              height: getProportionateScreenHeight(100),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  image: DecorationImage(
+                      image: NetworkImage("https://picsum.photos/200"))),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: SizeConfig.screenWidth * 0.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(tutorName),
+                  Text(subject + " Expert"),
+                  Text(location),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_rate,
+                        size: 16,
+                        color: Colors.redAccent.shade400,
+                      ),
+                      Icon(
+                        Icons.star_rate,
+                        size: 16,
+                        color: Colors.redAccent.shade400,
+                      ),
+                      Icon(
+                        Icons.star_rate,
+                        size: 16,
+                        color: Colors.redAccent.shade400,
+                      ),
+                      Icon(
+                        Icons.star_rate,
+                        size: 16,
+                        color: Colors.redAccent.shade400,
+                      ),
+                      Icon(
+                        Icons.star_rate,
+                        size: 16,
+                        color: Colors.redAccent.shade400,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
